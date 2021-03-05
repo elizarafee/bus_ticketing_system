6944d556541ba810f7e0489f872e74f1f0d51344
Eliza Ahmed <elizarafee@gmail.com>
2021-03-05 15:48:29 +0000

    a model named bus created and migrated

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Bus(models.Model):
+    bus_name = models.CharField(max_length=60)
+    bus_code = models.CharField(max_length=10)
+    
