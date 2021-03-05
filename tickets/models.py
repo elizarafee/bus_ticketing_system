b8f0e68021a10fc27915e8a48dfb04d7124563c8
Eliza Ahmed <elizarafee@gmail.com>
2021-03-05 16:47:01 +0000

    added string representations of every model

--- tickets/models.py
+++ tickets/models.py
@@ -5,10 +5,16 @@ from phone_field import PhoneField
 class Bus(models.Model):
     bus_name = models.CharField(max_length=60)
     bus_code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"Bus Name: {self.bus_name}, Bus Code: {self.bus_code}"
     
 class Bus_Stop(models.Model):
     bus_stop_name = models.CharField(max_length=60)
     bus_stop_code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"Bus Stop Name: {self.bus_stop_name}, Bus Stop Code: {self.bus_stop_code}"
 
 class Ticket(models.Model):
     pessenger_name = models.CharField(max_length=50)
@@ -19,3 +25,6 @@ class Ticket(models.Model):
     bus_code = models.CharField(max_length=10)
     date = models.DateField(null=True, blank=True)
     time = models.TimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"Pessenger Name: {self.pessenger_name}, Phone Number: {self.phone_number} ,Email: {self.email}, Departure: {self.departure}, Arrival: {self.arrival}, Bus Code: {self.bus_code} ,Date: {self.date}, Time: {self.time}"
