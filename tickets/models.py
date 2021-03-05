2db28f10f5e563ca547ab33c63d1e75b803a9800
Eliza Ahmed <elizarafee@gmail.com>
2021-03-05 16:36:31 +0000

    a model named ticket created and migrated

--- tickets/models.py
+++ tickets/models.py
@@ -9,3 +9,13 @@ class Bus(models.Model):
 class Bus_Stop(models.Model):
     bus_stop_name = models.CharField(max_length=60)
     bus_stop_code = models.CharField(max_length=10)
+
+class Ticket(models.Model):
+    pessenger_name = models.CharField(max_length=50)
+    phone_number = PhoneField(null=False, blank=False, unique=True)
+    email = models.EmailField(max_length=254)
+    departure = models.CharField(max_length=50)
+    arrival = models.CharField(max_length=50)
+    bus_code = models.CharField(max_length=10)
+    date = models.DateField(null=True, blank=True)
+    time = models.TimeField(null=True, blank=True)
