de7a752a482a48325970ad8d01316a96bffb1d8c
Eliza Ahmed <elizarafee@gmail.com>
2021-03-05 22:18:27 +0000

    altered models

--- tickets/models.py
+++ tickets/models.py
@@ -20,11 +20,12 @@ class Ticket(models.Model):
     pessenger_name = models.CharField(max_length=50)
     phone_number = PhoneField(null=False, blank=False, unique=True)
     email = models.EmailField(max_length=254)
-    departure = models.CharField(max_length=50)
-    arrival = models.CharField(max_length=50)
-    bus_code = models.CharField(max_length=10)
+    departure = models.ForeignKey(Bus_Stop, on_delete=models.CASCADE, related_name="r_departure")
+    arrival = models.ForeignKey(Bus_Stop, on_delete=models.CASCADE, related_name="r_arrival")
+    bus_name = models.ForeignKey(Bus, on_delete=models.CASCADE, related_name="r_bus_name", default='Bus')
+    bus_code = models.ForeignKey(Bus, on_delete=models.CASCADE, related_name="r_bus_code", default='Bus Code')
     date = models.DateField(null=True, blank=True)
     time = models.TimeField(null=True, blank=True)
 
     def __str__(self):
-        return f"Pessenger Name: {self.pessenger_name}, Phone Number: {self.phone_number} ,Email: {self.email}, Departure: {self.departure}, Arrival: {self.arrival}, Bus Code: {self.bus_code} ,Date: {self.date}, Time: {self.time}"
+        return f"Pessenger Name: {self.pessenger_name}, Phone Number: {self.phone_number} ,Email: {self.email}, Departure: {self.departure}, Arrival: {self.arrival}, Bus Code: {self.bus_code}, Bus Code: {self.bus_code}, Date: {self.date}, Time: {self.time}"
