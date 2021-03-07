17d7052f6e837ede8f1ba6d64f41d10da199fe84
Eliza Ahmed <elizarafee@gmail.com>
2021-03-07 09:46:09 +0000

    created superuse by createsuperuser and enabled admin to manipulate bus, bus_stop and ticket models

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Bus, Bus_Stop, Ticket
 
 # Register your models here.
+admin.site.register(Bus)
+admin.site.register(Bus_Stop)
+admin.site.register(Ticket)
