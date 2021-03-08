b1799e3ddc1ac8f07f9e0f69f70c21da1bb3b39b
Eliza Ahmed <elizarafee@gmail.com>
2021-03-08 10:01:21 +0000

    urls.py file is re-arranged

--- tickets/urls.py
+++ tickets/urls.py
@@ -4,9 +4,11 @@ from . import views
 app_name = "tickets"
 urlpatterns = [
     path("buses/", views.bus_index, name="bus_index"),
-    path("busstops/", views.bus_stop_index, name="bus_stop_index"),
-    path("tickets/", views.ticket_index, name="ticket_index"),
     path("bus/<str:bus_id>", views.bus, name="bus"),
+
+    path("busstops/", views.bus_stop_index, name="bus_stop_index"),
     path("busstop/<str:bus_stop_id>", views.bus_stop, name="busstop"),
+
+    path("tickets/", views.ticket_index, name="ticket_index"),
     path("ticket/<str:ticket_id>", views.ticket, name="ticket"),
 ]
