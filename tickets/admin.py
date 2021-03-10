14f9e4e9a0d7f42c130975f9895ebb50aeddb505
Eliza Ahmed <elizarafee@gmail.com>
2021-03-10 00:51:26 +0000

    customised admin panel

--- tickets/admin.py
+++ tickets/admin.py
@@ -2,7 +2,20 @@ from django.contrib import admin
 from .models import Bus, Bus_Stop, Ticket, Passenger
 
 # Register your models here.
-admin.site.register(Bus)
-admin.site.register(Bus_Stop)
-admin.site.register(Ticket)
-admin.site.register(Passenger)
+class BusAdmin(admin.ModelAdmin):
+    list_display = ("id", "bus_name", "bus_code")
+
+class bus_stopAdmin(admin.ModelAdmin):
+    list_display = ("id", "bus_stop_name", "bus_stop_code")
+
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ("id", "departure", "arrival", "bus", "date", "time")
+
+class PassengerAdmin(admin.ModelAdmin):
+    list_display = ("id", "passenger_name", "phone_number", "email")
+    filter_horizontal = ("ticket",)
+
+admin.site.register(Bus, BusAdmin)
+admin.site.register(Bus_Stop, bus_stopAdmin)
+admin.site.register(Ticket, TicketAdmin)
+admin.site.register(Passenger, PassengerAdmin)
